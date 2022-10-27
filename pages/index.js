110dcc8a2b698ef6b19576a4423bdfc5a97e9399
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-27 15:21:47 +0530

    Adding header component in home page.

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
-import MeetupList from '../components/meetups/MeetupList';
+import Layout from "../components/layout/Layout";
+import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
-    {
-        id : 'a1',
-        title : "A First Meetup!",
-        image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg',
-        address : 'some address 5, 12345 some city',
-        description : "This is a first meetup!"
-    },
-    {
-        id : 'a2',
-        title : "A Second Meetup!",
-        image : 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg',
-        address : 'some address 5, 12345 some city',
-        description : "This is a second meetup!"
-    },
-]
+  {
+    id: "a1",
+    title: "A First Meetup!",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 5, 12345 some city",
+    description: "This is a first meetup!",
+  },
+  {
+    id: "a2",
+    title: "A Second Meetup!",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "some address 5, 12345 some city",
+    description: "This is a second meetup!",
+  },
+];
 
 function HomePage() {
-    return <MeetupList meetups={DUMMY_MEETUPS} />
+  return (
+    <Layout>
+      <MeetupList meetups={DUMMY_MEETUPS} />
+    </Layout>
+  );
 }
 
 export default HomePage;
