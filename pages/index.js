6aa47f3731f0d987ea53f8befcd4771740a8adcc
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-28 10:16:34 +0530

    This code give SEO problem because first time render not contain any meetups data and nextjs convert first snapshot as static page.

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -20,9 +21,18 @@ const DUMMY_MEETUPS = [
 ];
 
 function HomePage() {
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    // may be fetching data from server
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, [])
+
   return (
-    <MeetupList meetups={DUMMY_MEETUPS} />
+    <MeetupList meetups={loadedMeetups} />
   );
 }
 
 export default HomePage;
+
+// NOTE : problem with this code is that in the initial load the meetups contain empty array so page render without meetups data, then next convert it initial state as a static page, after this useEffect run then change empty array to fetched data and reload meaningful page, but the static page contain only first state snapshot so this is a problem for SEO.
