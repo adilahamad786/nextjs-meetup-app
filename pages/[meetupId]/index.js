a54cccb349357299d6a2b6843a803640377a01b9
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-30 13:31:02 +0530

    Generate static page with respective content according to url meetups id.

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,32 +1,37 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
-function MeetupDetailsPage() {
+function MeetupDetailsPage(props) {
   return (
     <MeetupDetails
-      key='a1'
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg"
-      title="A First Meetup"
-      address="Some street 5, some city"
-      description="The meetup description."
+      key={props.meetup.id}
+      image={props.meetup.image}
+      title={props.meetup.title}
+      address={props.meetup.address}
+      description={props.meetup.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+
+  const client = await MongoClient.connect("mongodb+srv://Ju123Atla:JEMUzqtQk6MI1aJi@cluster0.rm8ojbq.mongodb.net/meetups?retryWrites=true&w=majority");
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, {_id : 1}).toArray();
+
+  client.close();
+
   return {
     fallback : false, // that means we define all static paths id inside the path array if path id not match inside the given paths array then simply give a 404 page bydefault, if we put as 'true' then we simple say if come any page id which are not contain in paths array then this render automatically according page.
-    paths : [ // defiend all dynamic paths id hear..
-      {
-        params : {
-          meetupId : 'a1'
-        }
-      },
-      {
-        params : {
-          meetupId : 'a2'
-        }
+    paths : meetups.map(meetup => ({ // generate dynamically paths using id's
+      params : {
+        meetupId : meetup._id.toString()
       }
-    ]
+    }))
   };
 };
 
@@ -34,15 +39,24 @@ export async function getStaticProps(context) {
   const meetupId = context.params.meetupId; // fetching meetupId form url params.
 
   // fetching meetups data for particular meetup (single meetup).
+  const client = await MongoClient.connect("mongodb+srv://Ju123Atla:JEMUzqtQk6MI1aJi@cluster0.rm8ojbq.mongodb.net/meetups?retryWrites=true&w=majority");
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({_id : ObjectId(meetupId)});
+
+  client.close();
 
   return {
     props : {
-      meetups : {
-        id : meetupId,
-        image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1024px-Stadtbild_M%C3%BCnchen.jpg",
-        title : "A First Meetup",
-        address : "Some street 5, some city",
-        description : "The meetup description."
+      meetup : {
+        id : selectedMeetup._id.toString(),
+        image : selectedMeetup.image,
+        title : selectedMeetup.title,
+        address : selectedMeetup.address,
+        description : selectedMeetup.description
       }
     }
   };
